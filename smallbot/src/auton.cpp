de5b8285f49ab1a1fd4d2a5bd5f7addd093a7b87
Kevin Qu <kevinqu2004@gmail.com>
2024-02-19 10:33:18 -0500

    simple smallbot skills auton

--- smallbot/src/auton.cpp
+++ smallbot/src/auton.cpp
@@ -10,11 +10,18 @@ void auton(){
 
     chassis->moveDistance(2_ft);
     intake();
-    chassis->moveDistance(-24_in);
-    chassis->turnAngle(90_deg);
+    chassis->moveDistance(24_in);
+    chassis->turnAngle(-90_deg);
     chassis->moveDistance(60_in);
     chassis->turnAngle(90_deg);
     score();
+    chassis->moveDistance(12_in);
+    for(int i = 0; i < 5; i++){
+        chassis->moveDistance(-12_in);
+        chassis->moveDistance(12_in);
+        score();
+    }
+
     //chassis->moveDistance(12_in);
 
 }
