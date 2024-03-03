872129b348e0955f00fd525473ecce1acf252e51
Kevin Qu <kevinqu2004@gmail.com>
2024-03-03 18:20:50 -0500

    added slow drive mode for increased accuracy

--- largebot/src/driveControl.cpp
+++ largebot/src/driveControl.cpp
@@ -1,10 +1,21 @@
 #include "main.h"
 #define max_analog 128.0 //maximum analog signal, used for scaling when driver input exceeds said value
+bool slowDrive = false;
 
 void driveControl(){
     // drive train control code (for driver control)
     int forward = - master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
     int lateral = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+    
+    if(slowDrive){
+        forward *= 0.5;
+        lateral *= 0.5;
+    }
+    if(master.get_digital(pros::E_CONTROLLER_DIGITAL_X)){
+        slowDrive = !slowDrive;
+        pros::delay(20);
+    }
+
     int left = forward - lateral;
     int right = - forward - lateral;
 
