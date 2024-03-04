c320439f33b14aa03c2a32accc87326796476d24
Kevin Qu <kevinqu2004@gmail.com>
2024-03-04 14:34:48 -0500

    tuned smallbot auton

--- smallbot/src/auton.cpp
+++ smallbot/src/auton.cpp
@@ -12,16 +12,16 @@ void auton(){
     chassis->moveDistance(2_ft);
     intake();
     chassis->moveDistance(24_in);
-    chassis->turnAngle(-90_deg);
-    chassis->moveDistance(24_in);
-    chassis->turnAngle(-90_deg);
-    chassis->moveDistance(12_in);
     chassis->turnAngle(90_deg);
+    chassis->moveDistance(24_in);
+    chassis->turnAngle(90_deg);
+    chassis->moveDistance(12_in);
+    chassis->turnAngle(-90_deg);
     flapPiston.set_value(true);
     //chassis->moveDistance(12_in);
-    pros::delay(40000); // -15 sec for actual tournament, 45 sec for skills
+    pros::delay(40000); // -15 sec for actual tournament, 40 sec for skills
     chassis->moveDistance(24_in);
-    chassis->turnAngle(90_deg);
+    chassis->turnAngle(-90_deg);
     chassis->moveDistance(20_in);
     score();
     flapPiston.set_value(false);
